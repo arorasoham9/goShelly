7a7b65854984f860636260fa9ed6f3c9da21147a
Anju Arora <anjumaggie@Anjus-MacBook-Air.local>
2022-06-13 10:47:49 +0530

    new file

--- client/tcpC.go
+++ client/tcpC.go
@@ -2,36 +2,13 @@ package main
 
 import (
 	"bufio"
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
-	"net"
+	"log"
 	"os"
 	"strings"
-	"crypto/tls"
-    // "crypto/x509"
-	"log"
 )
-
-func Dial(CONNECT string, config tls.Config) net.Conn {
-	c, err := tls.Dial("tcp", CONNECT, &config)
-	if err != nil {
-		fmt.Println(err)
-		return nil
-	}
-	return c
-}
-
-func execCommand(command string, length int32){
-
-}
-
-// func loadCertificate(config ) {
-// 	cert, err := tls.LoadX509KeyPair("certs/server.pem", "certs/server.key")
-//     if err != nil {
-//         log.Fatalf("server: loadkeys: %s", err)
-//     }
-//     config := tls.Config{Certificates: []tls.Certificate{cert}}
-//     config.Rand = rand.Reader
-// }
 
 func main() {
 	arguments := os.Args
@@ -39,26 +16,36 @@ func main() {
 		fmt.Println("Please provide host:port")
 		return
 	}
+
 	cert, err := tls.LoadX509KeyPair("certs/client.pem", "certs/client.key")
-    if err != nil {
-        log.Fatalf("server: loadkeys: %s", err)
-    }
-    config := tls.Config{Certificates: []tls.Certificate{cert}, InsecureSkipVerify: true}
-
-	CONNECT := arguments[1]
-	c := Dial(CONNECT, config)
-	if c == nil {
-		os.Exit(1)
+	if err != nil {
+		log.Fatalf("SSL Key Error: %s", err)
 	}
+	config := tls.Config{Certificates: []tls.Certificate{cert}, InsecureSkipVerify: true}
+	conn, err := tls.Dial("tcp", arguments[1], &config)
+	if err != nil {
+		log.Fatalf("Client Dial Error: %s", err)
+	}
+	defer conn.Close()
+	log.Println("Client Connected to: ", conn.RemoteAddr())
 
-	fmt.Println("Connected to server: ",strings.Split(c.RemoteAddr().String(), ":")[0]) 
-	reader := bufio.NewReader(c)
+	state := conn.ConnectionState()
+	for _, v := range state.PeerCertificates {
+		fmt.Println(x509.MarshalPKIXPublicKey(v.PublicKey))
+		fmt.Println(v.Subject)
+	}
+	log.Println("client: handshake: ", state.HandshakeComplete)
+	log.Println("client: mutual: ", state.NegotiatedProtocolIsMutual)
+
+
+	reader := bufio.NewReader(conn)
 	for {
 		text, _ := reader.ReadString('\n')
 		fmt.Printf(strings.Split(strings.TrimSpace(string(text)), "\n")[0] + "\n")
-		if strings.TrimSpace(string(text)) == "STOP" || strings.TrimSpace(string(text)) == "exit" {
+		if strings.TrimSpace(string(text)) == "stop" || strings.TrimSpace(string(text)) == "exit" {
 			fmt.Println("Disconnected from Server")
 			return
 		}
+
 	}
 }
